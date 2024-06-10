db6eb3211f3f722e5e70add00a37e0638517ca30
Luiz Henrique Costa <luizhrique97@gmail.com>
2024-06-09 23:35:40 -0300

    Entrega do projeto.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ABERTURA DE CONTA");
+
+        System.out.println("Olá! Qual seu nome? ");
+        String nome = sc.nextLine();
+
+        System.out.println(nome + ", digite o número da conta: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = sc.nextLine();
+
+        System.out.println("Quanto gostaria de depositar? ");
+        Double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque");
+
+        sc.close();
+
     }
 }
